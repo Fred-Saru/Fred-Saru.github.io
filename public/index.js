a30fe4314983c2d5ec979742de582237ebee20f4
Fred Alcide <frederic.alcide.infogene@axa.fr>
2023-12-23 17:04:47 +0100

    feat: ajout rotation portrait

--- public/index.js
+++ public/index.js
@@ -5,4 +5,15 @@
         const angle = (Math.random() * 30) - 15;
         el.style.setProperty('--footstep-rotation', angle +'deg');
     });
+
+    const choixpeau = document.querySelector(".choixpeau");
+    choixpeau.addEventListener("mousedown", () => {
+        const roulette = document.querySelector(".roulette");
+        roulette.classList.add("play");
+    });
+
+    choixpeau.addEventListener("mouseup", () => {
+        const roulette = document.querySelector(".roulette");
+        roulette.classList.remove("play");
+    });
 })();
